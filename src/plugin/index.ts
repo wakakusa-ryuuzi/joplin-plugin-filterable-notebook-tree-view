25fcc9e12ed991b63ca07392131cd32698bf935b
wakakusa ryuuzi <wakakusa.ryuuzi@gmail.com>
2026-02-12 01:01:09 +0900

    style: editorconfig入れて修正

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,8 +1,8 @@
 import joplin from 'api';
 
 joplin.plugins.register({
-	onStart: async function() {
-		// eslint-disable-next-line no-console
-		console.info('Hello world. Test plugin started!');
-	},
+  onStart: async function() {
+    // eslint-disable-next-line no-console
+    console.info('Hello world. Test plugin started!');
+  },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,23 @@ import path from 'path';
 const rootDir = path.resolve(__dirname, 'src/webview');
 
 export default defineConfig({
-	plugins: [vue()],
-	root: rootDir,
-	build: {
-		outDir: path.resolve(__dirname, 'dist-webview'),
-		emptyOutDir: true,
-		rollupOptions: {
-			input: path.resolve(rootDir, 'webview.ts'),
-			output: {
-				entryFileNames: 'webview.js',
-				format: 'iife',
-				name: 'webview',
-			},
-		},
-	},
-	server: {
-		middlewareMode: false,
-		port: 5173,
-		cors: true,
-	},
+  plugins: [vue()],
+  root: rootDir,
+  build: {
+    outDir: path.resolve(__dirname, 'dist-webview'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(rootDir, 'webview.ts'),
+      output: {
+        entryFileNames: 'webview.js',
+        format: 'iife',
+        name: 'webview',
+      },
+    },
+  },
+  server: {
+    middlewareMode: false,
+    port: 5173,
+    cors: true,
+  },
 });
